236d1a012f0113338109e146bfb48bbe7ace12c5
Asmaa_magd_helali <asmaamagdhelali@gmail.com>
2018-04-01 22:08:02 +0200

    resources finished

--- app/src/main/java/com/example/android/aqarmaptask/activities/MainActivity.java
+++ app/src/main/java/com/example/android/aqarmaptask/activities/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.aqarmaptask.activities;
 
+import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.android.aqarmaptask.R;
@@ -11,29 +15,71 @@ import com.example.android.aqarmaptask.models.locations.locationsResponse.Locati
 import com.example.android.aqarmaptask.models.prices.pricesResponse.PricesResponse;
 import com.example.android.aqarmaptask.models.propertyTypes.propertyTypesResponse.PropertyTypesResponse;
 import com.example.android.aqarmaptask.models.sections.SectionsResponse.SectionsResponse;
+import com.example.android.aqarmaptask.utils.network.NetworkUtil;
 import com.example.android.aqarmaptask.utils.webservice.MyTask;
+import com.isapanah.awesomespinner.AwesomeSpinner;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import co.ceryle.radiorealbutton.RadioRealButton;
+import co.ceryle.radiorealbutton.RadioRealButtonGroup;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Function4;
 import io.reactivex.schedulers.Schedulers;
 
 
 public class MainActivity extends AppCompatActivity {
     private CompositeDisposable disposable;
+    @BindView(R.id.progressbar)
+    ContentLoadingProgressBar loadingProgressBar;
+    @BindView(R.id.iv_no_network)
+    ImageView ivNoNetwork;
+    @BindView(R.id.btn_search)
+    Button btnSearch;
+    @BindView(R.id.sp_locations)
+    AwesomeSpinner spLocations;
+    @BindView(R.id.sp_sections)
+    AwesomeSpinner spSections;
+    @BindView(R.id.sp_type)
+    AwesomeSpinner spType;
+    @BindView(R.id.sp_maxPrices)
+    AwesomeSpinner spMaxPrice;
+    @BindView(R.id.sp_minPrices)
+    AwesomeSpinner spMinPrice;
+    @BindView(R.id.rg_search)
+    RadioRealButtonGroup rgSearch;
+    @BindView(R.id.rb_rent)
+    RadioRealButton rbRent;
+    @BindView(R.id.rb_sale)
+    RadioRealButton rbSale;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        loadingProgressBar.setVisibility(View.VISIBLE);
+        checkNetwork();
+    }
 
-        callallFilterLists();
+    private void checkNetwork() {
+        switch (NetworkUtil.getConnectivityStatus(this)) {
+            case OFFLINE:
+                loadingProgressBar.setVisibility(View.GONE);
+                Toast.makeText(this, getString(R.string.offline), Toast.LENGTH_SHORT).show();
+                break;
+            case WIFI_CONNECTED_WITHOUT_INTERNET:
+                loadingProgressBar.setVisibility(View.GONE);
+                Toast.makeText(this, getString(R.string.offline), Toast.LENGTH_SHORT).show();
+                break;
+            case MOBILE_DATA_CONNECTED:
+            case WIFI_CONNECTED_WITH_INTERNET:
+                callallFilterLists();
+                break;
+        }
     }
 
     private void callallFilterLists() {
@@ -60,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void handleResults(MergedSearchListsResponses mergedSearchListsResponses) {
+        loadingProgressBar.setVisibility(View.GONE);
         if (mergedSearchListsResponses.getSectionsResponse() != null) {
             Toast.makeText(this, "result successfully isa", Toast.LENGTH_SHORT).show();
         } else {
@@ -68,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void handleError(Throwable t) {
+        loadingProgressBar.setVisibility(View.GONE);
         Log.e("Observer", "" + t.toString());
         Toast.makeText(this, "ERROR IN GETTING COUPONS",
                 Toast.LENGTH_LONG).show();
